9721b36031e55104346dc2114caeb4ec1adeb6e9
Leigh Baugham <leigh.baugham@gmail.com>
2019-04-29 15:36:50 -0400

    issues galore

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :trips
   resources :passports
   resources :users
+
+  post "/login", to: "users#login"
+  # get "/signup", to: "users#create"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
