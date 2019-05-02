81e74fd4f0e676847f736f1aa9997b7e1b4a3db6
Leigh Baugham <leigh.baugham@gmail.com>
2019-05-02 11:28:32 -0400

    added dependency on destroy for trips adn users

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
 
     has_many :passports
-    has_many :users, through: :passports
+    has_many :users, through: :passports, :dependent => :destroy
     belongs_to :user
 
 end
